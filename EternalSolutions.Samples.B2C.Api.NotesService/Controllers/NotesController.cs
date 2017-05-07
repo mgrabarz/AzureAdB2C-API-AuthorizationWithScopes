77fe3db6da3f5f73a26c2a99f1ef75fce1837e91
Marek Grabarz <m.grabarz@wp.pl>
2017-05-07 17:29:51 +0200

    Sample API implementation.

---
+++ EternalSolutions.Samples.B2C.Api.NotesService/Controllers/NotesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EternalSolutions.Samples.B2C.Api.NotesService.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Note1", "Note2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Note" + id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            Ok();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            Ok();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            StatusCode(418);
+        }
+    }
+}
